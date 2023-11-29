c055d852aa81670188d6e669b50246e3bc50fedf
Martin Ligabue <martinligabue@gmail.com>
2023-11-29 19:45:32 +0100

    tests

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # sieve-helper
 
-A description of this project.
+Sieve helper is a tool to help you write sieve scripts for [dovecot](https://www.dovecot.org/).
+
+## Installation
+### From packages
+* Arch Linux: [sieve-helper](https://aur.archlinux.org/packages/sieve-helper/)
+* Debian: [sieve-helper](https://packages.debian.org/sieve-helper)
+* Generic Linux: In the releases
+### From source
+* clone this repository
+* meson build
--- src/sieve_config_generator.py
+++ src/sieve_config_generator.py
@@ -5,7 +5,7 @@ class SieveUtilities():
     def generate_sieve_config(config):
         # config has shape [email, from_check, to_check, target_folder]
         out_string = "require \"fileinto\";"
-
+    #the following loop loops through the config array and adds the sieve rules to the out_string
         for c in config:
             email = c[0]
             from_check = c[1]
@@ -24,5 +24,10 @@ class SieveUtilities():
             out_string += "\";}"
 
         return out_string
+    # New version of sieve config where config is not an array, but an object with keys
+
+    def new_generate_sieve_config(config):
+        # config has attributes config.email config.from_check config.to_check config.target_folder
+        out_string = "require \"fileinto\";"
 
 
